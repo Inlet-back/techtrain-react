6298bffaaeb5f54fd314c87254b1d1e14c5e36b0
Ura <mikeneko41@icloud.com>
2025-03-04 02:54:26 +0900

    二重の原因解消

--- threads/src/pages/ThreadPage.tsx
+++ threads/src/pages/ThreadPage.tsx
@@ -1,13 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "../index.css";
 import Header from "./components/Header";
 import { useLocation, useParams } from "react-router-dom";
 import { ThreadWithPost } from "types/thread";
 import Button from "./components/Button";
+import { set, z } from "zod";
+import { PostSchema } from "@lib/post";
 
 interface ThreadName {
   threadName: string;
 }
+type PostPosts = z.infer<typeof PostSchema>;
 
 function ThreadPage() {
   const location = useLocation();
@@ -15,9 +18,9 @@ function ThreadPage() {
 
   const { id } = useParams<{ id: string }>();
   const [thread, setThread] = useState<ThreadWithPost | null>(null);
-  const [post, setPost] = useState<string>("");
   const [offset, setOffset] = useState<number>(0);
   const [hasMore, setHasMore] = useState<boolean>(true);
+  const postInputRef = useRef<HTMLInputElement>(null);
 
   const fetchPosts = async () => {
     const response = await fetch(
@@ -27,20 +30,33 @@ function ThreadPage() {
       console.log("サーバーエラー");
       return;
     }
-    const data :ThreadWithPost = await response.json();
+    const data: ThreadWithPost = await response.json();
     if (data.posts.length < 10) {
       setHasMore(false);
     }
 
-    setThread((prev) => ({
-      ...prev!,
-      posts: [...(prev?.posts || []), ...data.posts],
-    }));
+    
+    setThread((prev) => {
+      if (offset === 0) {
+        return {
+          ...prev!,
+          posts: data.posts,
+        };
+      } else {
+        return {
+          ...prev!,
+          posts: [...(prev?.posts || []), ...data.posts],
+        };
+      }
+    });
     console.log(data);
   };
 
   const onSubmitPost = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const post = postInputRef.current?.value;
+    if (!post) return;
+
     const response = await fetch(
       `https://railway.bulletinboard.techtrain.dev/threads/${id}/posts`,
       {
@@ -55,13 +71,15 @@ function ThreadPage() {
       console.error("サーバーエラー");
       return;
     }
-    setPost("");
+    postInputRef.current.value = ""; // フォームのリセット
+ 
     fetchPosts();
+    
   };
 
   useEffect(() => {
     fetchPosts();
-  }, [id,offset]);
+  }, [id, offset]);
 
   return (
     <div>
@@ -71,23 +89,22 @@ function ThreadPage() {
           <label style={{ display: "block", fontSize: "30px", fontWeight: "bold", marginBottom: "8px" }}>
             {threadName}
           </label>
-          {thread && thread.posts.map((post,index) => (
-            <div key={post.id+index} style={{ padding: "16px", border: "2px solid black", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", transition: "background-color 0.3s" }}>
+          {thread && thread.posts.map((post, index) => (
+            <div key={`${post.id}-${index}`} style={{ padding: "16px", border: "2px solid black", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", transition: "background-color 0.3s" }}>
               <h2 style={{ fontSize: "20px", fontWeight: "600" }}>{post.post}</h2>
             </div>
           ))}
-           {hasMore && (
+          {hasMore && (
             <button onClick={() => setOffset((prevOffset) => prevOffset + 10)} style={{ marginTop: "16px", padding: "12px 24px", backgroundColor: "blue", color: "white", border: "none", borderRadius: "4px", fontSize: "16px" }}>
               もっと見る
             </button>
           )}
         </div>
-        <div style={{ flex: 1 ,paddingTop: "200px", maxWidth: "1200px", margin: "0 auto", padding: "16px", display: "flex", gap: "16px"}}>
+        <div style={{ flex: 1, paddingTop: "200px", maxWidth: "1200px", margin: "0 auto", padding: "16px", display: "flex", gap: "16px" }}>
           <form onSubmit={onSubmitPost}>
             <input
               type="text"
-              value={post}
-              onChange={(e) => setPost(e.target.value)}
+              ref={postInputRef}
               placeholder="投稿しよう"
               style={{ width: "100%", padding: "12px", border: "1px solid #ccc", borderRadius: "4px", fontSize: "16px" }}
               required
